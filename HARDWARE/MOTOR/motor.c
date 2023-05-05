7c17b8251428b9714387e9d5279858829d6d31f0
nypyp <nypypbro@gmail.com>
2023-05-05 19:38:13 +0800

    添加at8236电机驱动 主要修改motor.c motor.h

--- HARDWARE/MOTOR/motor.c
+++ HARDWARE/MOTOR/motor.c
@@ -4,36 +4,30 @@
 void Motor_Init(void)
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE); //使能端口时钟
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE); //使能端口时钟
-	
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4|GPIO_Pin_5;	//端口配置
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;      //推挽输出
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;     //50M
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-	
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4|GPIO_Pin_5;	//端口配置
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;      //推挽输出
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;     //50M
-	GPIO_Init(GPIOC, &GPIO_InitStructure);
-}
-
-void MOTOR_PWM_Init(u16 arr, u16 psc)
-{
-	//初始化结构体
-	GPIO_InitTypeDef GPIO_InitStructure;
-	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
-	TIM_OCInitTypeDef  TIM_OCInitStructure;
-	
-	//使能时钟
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA  | RCC_APB2Periph_AFIO, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB  | RCC_APB2Periph_AFIO, ENABLE);
 	
 	//设置GPIO初始化结构体
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6|GPIO_Pin_7; //TIM3_CH1|TIM3_CH2
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;  //复用推挽输出
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);//初始化GPIO
+	
+	//设置GPIO初始化结构体
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1; //TIM3_CH1|TIM3_CH2
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;  //复用推挽输出
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);//初始化GPIO
+}
+
+void MOTOR_PWM_Init(u16 arr, u16 psc)
+{
+	//初始化结构体
+	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+	TIM_OCInitTypeDef  TIM_OCInitStructure;
+	
+	//使能时钟
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
 	
 	//初始化TIM3
 	TIM_TimeBaseStructure.TIM_Period = arr; //设置在下一个更新事件装入活动的自动重装载寄存器周期的值
@@ -46,12 +40,18 @@ void MOTOR_PWM_Init(u16 arr, u16 psc)
 	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1; //选择定时器模式:TIM脉冲宽度调制模式2
  	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable; //比较输出使能
 	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High; //输出极性:TIM输出比较极性高
+	TIM_OCInitStructure.TIM_Pulse = 0;                            		//设置待装入捕获比较寄存器的脉冲值
 	TIM_OC1Init(TIM3, &TIM_OCInitStructure);  //初始化TIM3_OC1
 	TIM_OC2Init(TIM3, &TIM_OCInitStructure);  //根据T指定的参数初始化外设TIM3 OC2
+	TIM_OC3Init(TIM3, &TIM_OCInitStructure);
+	TIM_OC4Init(TIM3, &TIM_OCInitStructure);
 	
 	TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);  //使能TIM3在CCR2上的预装载寄存器
 	TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);
+	TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);
+	TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
 
+	TIM_ARRPreloadConfig(TIM3, ENABLE);
 	TIM_Cmd(TIM3, ENABLE);  //使能TIM3
 }
 
@@ -59,14 +59,14 @@ void Set_Pwm(int motor1, int motor2)
 {
 	if(motor1>0 && motor1>100) motor1 = 100;//限幅
 	if(motor1<0 && motor1<-100) motor1 = -100;
-	if(motor1>0) AIN1 = 0, AIN2 = 1;
-	else         AIN1 = 1, AIN2 = 0;
-	PWMA = myabs(motor1)*69;
+	if(motor1<0)	PWMAIN1 = 7200,PWMAIN2=myabs(motor1)*69; //motor 范围为[0,100], 乘上“69”限幅因子后，PWM范围为[0,6900]
+	else			PWMAIN2 = 7200,PWMAIN1=myabs(motor1)*69;
+	
 	if(motor2>0 && motor2>100) motor2 = 100;//限幅
 	if(motor2<0 && motor2<-100) motor2 = -100;
-	if(motor2>0) BIN1 = 0, BIN2 = 1;
-	else         BIN1 = 1, BIN2 = 0;
-	PWMB = myabs(motor2)*69; //motor 范围为[0,100], 乘上“69”限幅因子后，PWM范围为[0,6900]
+	if(motor2<0)	PWMBIN1 = 7200,PWMBIN2=myabs(motor2)*69; //驱动AT8236输入IN1:1 IN2:PWM 为慢衰减模式正转，详情查看芯片数据手册
+	else			PWMBIN2 = 7200,PWMBIN1=myabs(motor2)*69;
+ 
 }
 
 int myabs(int num)
--- HARDWARE/MOTOR/motor.h
+++ HARDWARE/MOTOR/motor.h
@@ -2,12 +2,14 @@
 #define __MOTOR_H
 #include "sys.h"
 
-#define PWMA   TIM3->CCR1  
-#define AIN1   PAout(5)
-#define AIN2   PAout(4)
-#define PWMB   TIM3->CCR2 
-#define BIN1   PCout(5)
-#define BIN2   PCout(4)
+#define PWMAIN1   TIM3->CCR1
+#define PWMAIN2   TIM3->CCR2
+#define AIN1   PAout(6)
+#define AIN2   PAout(7)
+#define PWMBIN1   TIM3->CCR3
+#define PWMBIN2   TIM3->CCR4
+#define BIN1   PBout(0)
+#define BIN2   PBout(1)
 
 void Motor_Init(void);
 void MOTOR_PWM_Init(u16 arr, u16 psc);
